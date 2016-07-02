3d3085daa86d1fb8e9ebcb38293df728711ff77b
cliversoft <sergey.stoyan@gmail.com>
2016-07-03 01:02:36 +0300

    .

--- CliverBotCustomization.cs
+++ CliverBotCustomization.cs
@@ -81,19 +81,16 @@ Developed by: www.cliversoft.com";
 
             vs.Clear();
 
-            if (File.Exists(FileWriter.This.FilePath))
-            {
-                FileReader fr = new FileReader(FileWriter.This.FilePath, Cliver.Bot.Properties.Output.Default.OutputFieldSeparator, null, true, null, true);
-                prev_row = fr.ReadAll().LastOrDefault();
-            }
+            if (File.Exists(last_prices_file))
+                urls2old_price = Cliver.SerializationRoutines.Json.Get<Dictionary<string, double>>(File.ReadAllText(last_prices_file));
 
-            {
-                FileReader fr = new FileReader(Custom.Default.UsersFile, Cliver.Bot.Properties.Input.Default.InputFieldSeparator);
-                for (FileReader.Row r = fr.ReadLine(); r != null; r = fr.ReadLine())
-                    users2ui[r["User"]] = new UserInfo() { Mobile = r["Mobile"], SmsGateway = r["SmsGateway"] };
-            }
+            FileReader fr = new FileReader(Custom.Default.UsersFile, Cliver.Bot.Properties.Input.Default.InputFieldSeparator);
+            for (FileReader.Row r = fr.ReadLine(); r != null; r = fr.ReadLine())
+                users2ui[r["User"]] = new UserInfo() { Mobile = r["Mobile"], SmsGateway = r["SmsGateway"] };
         }
-        static FileReader.Row prev_row;
+        static readonly string last_prices_file = Log.WorkDir + "\\" + "last_prices.txt";
+        static Dictionary<string, double> urls2old_price = null;
+        static Dictionary<string, double> urls2price = new Dictionary<string,double>();
         static Dictionary<string, UserInfo> users2ui = new Dictionary<string, UserInfo>();
         public class UserInfo
         {
@@ -103,6 +100,7 @@ Developed by: www.cliversoft.com";
 
         new static public void SessionClosing()
         {
+            File.WriteAllText(last_prices_file, Cliver.SerializationRoutines.Json.Get(urls2price));
             vs.Insert(0, DateTime.Now.ToShortTimeString());
             vs.Insert(0, DateTime.Now.ToShortDateString());
             FileWriter.This.PrepareAndWriteHtmlLine(vs.ToArray());
@@ -116,16 +114,25 @@ Developed by: www.cliversoft.com";
         {
             lock (vs)
             {
-                string f = hd != null ? hd.GetElementsByTagName("HTML")[0].OuterHtml : "";
-                DataSifter.Capture c = flights.Parse(f);
-                DataSifter.Capture fc = c.FirstOf("Flight");
-                //string route = PrepareField.Html.GetCsvField(fc.FirstValueOf("Route"));
-                price = Regex.Replace(FileWriter.Html.PrepareField(fc.FirstValueOf("Price")), @"\s", "");
-                DataSifter.Capture cu = CustomBot.url.Parse(url);
-                route = cu.FirstValueOf("Route");
-                if (string.IsNullOrWhiteSpace(route))
-                    route = cu.FirstValueOf("Route1") + "-" + cu.FirstValueOf("Route2");
-                route = cu.FirstValueOf("A") + "-" + route;
+                route = "";
+                price = "";
+                try
+                {
+                    string f = hd != null ? hd.GetElementsByTagName("HTML")[0].OuterHtml : "";
+                    DataSifter.Capture c = flights.Parse(f);
+                    DataSifter.Capture fc = c.FirstOf("Flight");
+                    //string route = PrepareField.Html.GetCsvField(fc.FirstValueOf("Route"));
+                    price = Regex.Replace(FileWriter.Html.PrepareField(fc.FirstValueOf("Price")), @"\s", "");
+                    DataSifter.Capture cu = CustomBot.url.Parse(url);
+                    route = cu.FirstValueOf("Route");
+                    if (string.IsNullOrWhiteSpace(route))
+                        route = cu.FirstValueOf("Route1") + "-" + cu.FirstValueOf("Route2");
+                    route = cu.FirstValueOf("A") + "-" + route;
+                }
+                catch(Exception e)
+                {
+                    Log.Error(e);
+                }
                 vs.Add(route);
                 vs.Add(price);
             }
@@ -151,7 +158,7 @@ Developed by: www.cliversoft.com";
                 //Log.Write("1");
                 //return;
                 CustomBot cb = (CustomBot)bc.Bot;
-                
+
                 string route;
                 string price;
 
@@ -166,47 +173,47 @@ Developed by: www.cliversoft.com";
 
                 string s2 = Regex.Replace(price, @"[^\d\.]", "", RegexOptions.IgnoreCase);
                 double p2 = double.Parse(s2);
+                urls2price[Url] = p2;
 
-                if (prev_row != null)
+                if (urls2old_price == null)
+                    return;
+
+                double p;
+                if (!urls2old_price.TryGetValue(Url, out p))
+                    return;
+
+                if (AlertFactor * p < p2)
+                    return;
+
+                List<MailMessage> mms = new List<MailMessage>();
+                mms.Add(
+                    new MailMessage(Custom.Default.Email, Custom.Default.Email)
+                    {
+                        Subject = "Flight Alert: " + route + " " + price,
+                        Body = route + " " + price
+                    }
+                );
+
+                foreach (string u in Users.Split(';'))
                 {
-                    for (int i = 1; i < prev_row.Count; i++)
-                        if (prev_row[i] == route)
+                    UserInfo ui;
+                    if (!users2ui.TryGetValue(u, out ui))
+                        throw new Bot.ProcessorException(ProcessorExceptionType.ERROR, "User " + u + " is not defined");
+                    mms.Add(
+                        new MailMessage(Custom.Default.Email, ui.Mobile + "@" + ui.SmsGateway)
                         {
-                            string s = Regex.Replace(prev_row[i + 1], @"[^\d\.]", "", RegexOptions.IgnoreCase);
-                            double p = double.Parse(s);
-                            if (AlertFactor * p >= p2)
-                            {
-                                List<MailMessage> mms = new List<MailMessage>();
-                                mms.Add(
-                                    new MailMessage(Custom.Default.Email, Custom.Default.Email)
-                                    {
-                                        Subject = "Flight Alert: " + route + " " + price,
-                                        Body = route + " " + price
-                                    }
-                                );
-
-                                foreach (string u in Users.Split(';'))
-                                {
-                                    UserInfo ui;
-                                    if (!users2ui.TryGetValue(u, out ui))
-                                        throw new Bot.ProcessorException(ProcessorExceptionType.ERROR, "User " + u + " is not defined");
-                                    mms.Add(
-                                        new MailMessage(Custom.Default.Email, ui.Mobile + "@" + ui.SmsGateway)
-                                        {
-                                            Subject = route + " " + price,
-                                            Body = route + " " + price
-                                        }
-                                    );
-                                }
-
-                                email(mms);
-                            }
+                            Subject = route + " " + price,
+                            Body = route + " " + price
                         }
+                    );
                 }
+
+                email(mms);
             }
 
             void email(List<MailMessage> mms)
             {
+               // return;
                 if (mms.Count < 1)
                     return;
                 try
@@ -239,3 +246,73 @@ Developed by: www.cliversoft.com";
         }
     }
 }
+
+
+/*
+Url	Users	AlertFactor
+https://www.google.com/flights/#search;f=DEN;t=LHR;d=2016-08-11;r=2016-08-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=CDG;d=2016-08-11;r=2016-08-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=MUC;d=2016-08-11;r=2016-08-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=FCO;d=2016-08-11;r=2016-08-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=DUB;d=2016-08-11;r=2016-08-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=MAD;d=2016-08-11;r=2016-08-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=FRA;d=2016-08-11;r=2016-08-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=TYO;d=2016-08-11;r=2016-08-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=PEK;d=2016-08-11;r=2016-08-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=HNL;d=2016-08-11;r=2016-08-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=LHR;d=2016-09-11;r=2016-09-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=CDG;d=2016-09-11;r=2016-09-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=MUC;d=2016-09-11;r=2016-09-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=FCO;d=2016-09-11;r=2016-09-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=DUB;d=2016-09-11;r=2016-09-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=MAD;d=2016-09-11;r=2016-09-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=FRA;d=2016-09-11;r=2016-09-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=TYO;d=2016-09-11;r=2016-09-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=PEK;d=2016-09-11;r=2016-09-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=HNL;d=2016-09-11;r=2016-09-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=LHR;d=2016-10-11;r=2016-10-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=CDG;d=2016-10-11;r=2016-10-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=MUC;d=2016-10-11;r=2016-10-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=FCO;d=2016-10-11;r=2016-10-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=DUB;d=2016-10-11;r=2016-10-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=MAD;d=2016-10-11;r=2016-10-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=FRA;d=2016-10-11;r=2016-10-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=TYO;d=2016-10-11;r=2016-10-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=PEK;d=2016-10-11;r=2016-10-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=HNL;d=2016-10-11;r=2016-10-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=TYS;d=2016-10-06;r=2016-10-09;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=TRI;d=2016-10-06;r=2016-10-09;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=LHR;d=2016-11-11;r=2016-11-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=CDG;d=2016-11-11;r=2016-11-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=MUC;d=2016-11-11;r=2016-11-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=FCO;d=2016-11-11;r=2016-11-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=DUB;d=2016-11-11;r=2016-11-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=MAD;d=2016-11-11;r=2016-11-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=FRA;d=2016-11-11;r=2016-11-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=TYO;d=2016-11-11;r=2016-11-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=PEK;d=2016-11-11;r=2016-11-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=HNL;d=2016-11-11;r=2016-11-25;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=LHR;d=2016-12-11;r=2016-12-28;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=CDG;d=2016-12-11;r=2016-12-28;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=MUC;d=2016-12-11;r=2016-12-28;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=FCO;d=2016-12-11;r=2016-12-28;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=DUB;d=2016-12-11;r=2016-12-28;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=MAD;d=2016-12-11;r=2016-12-28;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=FRA;d=2016-12-11;r=2016-12-28;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=TYO;d=2016-12-11;r=2016-12-28;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=PEK;d=2016-12-11;r=2016-12-28;a=UA;so=p	Larry	0.75
+https://www.google.com/flights/#search;f=DEN;t=HNL;d=2016-12-11;r=2016-12-28;a=UA;so=p	Larry	0.75
+
+*/
+
+/*
+User	Mobile	SmsGateway
+Larry	3038278500	txt.att.net
+Judy	3037755150	txt.att.net	
+Jerry C	3035214011	vtext.com
+Amy	3039314413	vtext.com
+Bruce	3039071794	vtext.com
+Jerry S	9314091320	vtext.com
+	
+
+*/
